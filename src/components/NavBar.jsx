6a0f0c6399aa81f00377a86196ededb4878bd787
javierezequielfarias <javierezequielfarias@gmail.com>
2024-07-18 11:44:06 -0300

    Primer Entrega Curso React

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { CartWidget } from './CarWidget';
+
+export const NavBar = () => (
+<>
+    <h1>Pet shop Online Goku</h1>
+    <ul>
+        <li><a href="">Alimentos</a></li>
+        <li><a href="">Indumentaria</a></li>
+        <li><a href="">Juguetes</a></li>
+    </ul>
+    <CartWidget />
+</>
+);
